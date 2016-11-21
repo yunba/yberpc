d7d0b0ccc7f1aa1f93f68f9db052f912f13bcef5
shdxiang <shdxiang@qq.com>
2016-11-21 11:46:34 +0800

    Rename some files

--- src/msgbus_rpc_proxy.erl
+++ src/msgbus_rpc_proxy.erl
@@ -1,181 +1,53 @@
-%%%-------------------------------------------------------------------
-%%% @author shdxiang
-%%% @copyright (C) 2016, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 08. Nov 2016 5:00 PM
-%%%-------------------------------------------------------------------
 -module(msgbus_rpc_proxy).
--author("shdxiang").
 
--behaviour(gen_server).
-
--compile({parse_transform, lager_transform}).
-
--include_lib("eunit/include/eunit.hrl").
+-behaviour(supervisor).
 
 %% API
--export([start_link/1]).
+-export([start_link/0,
+  start_server/1,
+  start_client/1,
+  set_handler/2,
+  rpc/2,
+  stop_server/1,
+  stop_client/1]).
 
-%% gen_server callbacks
--export([init/1,
-  handle_call/3,
-  handle_cast/2,
-  handle_info/2,
-  terminate/2,
-  code_change/3]).
+%% Supervisor callbacks
+-export([init/1]).
 
--define(SERVER, ?MODULE).
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
--record(state, {
-  sock,
-  handler
-}).
+%% ===================================================================
+%% API functions
+%% ===================================================================
 
-%%%===================================================================
-%%% API
-%%%===================================================================
+start_link() ->
+  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(start_link(Args :: term()) ->
-  {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
-start_link(Args) ->
-  gen_server:start_link(?MODULE, [Args], []).
+start_server(Url) ->
+  supervisor:start_child(?MODULE, [{server, Url}]).
 
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
+start_client(Url) ->
+  supervisor:start_child(?MODULE, [{client, Url}]).
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
--spec(init(Args :: term()) ->
-  {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term()} | ignore).
-init(Args) ->
-  ?debugFmt("init: ~p", [Args]),
-  [{Type, Url}] = Args,
-  case Type of
-    server ->
-      {ok, Sock} = enm:pull([{bind, Url}, raw]),
-      {ok, #state{sock = Sock}};
-    client ->
-      {ok, Sock} = enm:push([{connect, Url}, raw]),
-      {ok, #state{sock = Sock}};
-    Other ->
-      ?debugFmt("unknown type: ~p", [Other]),
-      {stop, unknown_type}
-  end.
+set_handler(Pid, Handler) ->
+  gen_server:call(Pid, {set_handler, Handler}).
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
-    State :: #state{}) ->
-  {reply, Reply :: term(), NewState :: #state{}} |
-  {reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
-  {noreply, NewState :: #state{}} |
-  {noreply, NewState :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
-  {stop, Reason :: term(), NewState :: #state{}}).
+rpc(Pid, Data) ->
+  gen_server:call(Pid, {rpc, Data}).
 
-handle_call({set_handler, Handler}, _From, State) ->
-  ?debugFmt("set_handler: ~p", [Handler]),
-  {reply, ok, State#state{handler = Handler}};
+stop_server(Pid) ->
+  supervisor:terminate_child(?MODULE, Pid).
 
-handle_call({rpc, Data}, _From, #state{sock = Sock} = State) ->
-  ?debugFmt("rpc: ~p", [Sock]),
-  ok = enm:send(Sock, Data),
-  {reply, ok, State};
+stop_client(Pid) ->
+  supervisor:terminate_child(?MODULE, Pid).
 
-handle_call(_Request, _From, State) ->
-  {reply, ok, State}.
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_cast(Request :: term(), State :: #state{}) ->
-  {noreply, NewState :: #state{}} |
-  {noreply, NewState :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term(), NewState :: #state{}}).
-handle_cast(_Request, State) ->
-  {noreply, State}.
+init([]) ->
+  {ok, {{simple_one_for_one, 0, 1},
+    [{client, {msgbus_rpc_proxy_gs, start_link, []},
+      temporary, 5000, worker, [msgbus_rpc_proxy_gs]}]}}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
--spec(handle_info(Info :: timeout() | term(), State :: #state{}) ->
-  {noreply, NewState :: #state{}} |
-  {noreply, NewState :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term(), NewState :: #state{}}).
-handle_info({nnpull, Sock, Data}, #state{sock = Sock, handler = Handler} = State) ->
-  ?debugFmt("receive a nnpull: ~p", [Sock]),
-  Handler ! {rpc_proxy_data, Data},
-  {noreply, State};
-handle_info(_Info, State) ->
-  {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
--spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
-    State :: #state{}) -> term()).
-terminate(_Reason, #state{sock = Sock} = _State) ->
-  ?debugMsg("terminate"),
-  enm:close(Sock),
-  ok.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
--spec(code_change(OldVsn :: term() | {down, term()}, State :: #state{},
-    Extra :: term()) ->
-  {ok, NewState :: #state{}} | {error, Reason :: term()}).
-code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
-
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
--- test/msgbus_rpc_proxy_tests.erl
+++ test/msgbus_rpc_proxy_tests.erl
@@ -18,25 +18,25 @@
 
 init_test() ->
   {ok, _Pid} = enm:start_link(),
-  msgbus_rpc_proxy_sup:start_link(),
+  msgbus_rpc_proxy:start_link(),
   ets:new(data, [set, named_table]).
 
 start_server_test() ->
-  {ok, Pid} = msgbus_rpc_proxy_sup:start_server("tcp://*:9000"),
+  {ok, Pid} = msgbus_rpc_proxy:start_server("tcp://*:9000"),
   ets:insert(data, {server_pid, Pid}).
 
 start_client_test() ->
-  {ok, Pid} = msgbus_rpc_proxy_sup:start_client("tcp://localhost:9000"),
+  {ok, Pid} = msgbus_rpc_proxy:start_client("tcp://localhost:9000"),
   ets:insert(data, {client_pid, Pid}).
 
 set_handler_test() ->
   [{server_pid, ServerPid}] = ets:lookup(data, server_pid),
   [{client_pid, ClientPid}] = ets:lookup(data, client_pid),
 
-  ok = msgbus_rpc_proxy_sup:set_handler(ServerPid, self()),
+  ok = msgbus_rpc_proxy:set_handler(ServerPid, self()),
 
   Data = <<"test">>,
-  ok = msgbus_rpc_proxy_sup:rpc(ClientPid, Data),
+  ok = msgbus_rpc_proxy:rpc(ClientPid, Data),
   receive
     {rpc_proxy_data, Data} -> ignore
   after
@@ -45,8 +45,8 @@ set_handler_test() ->
 
 stop_server_test() ->
   [{server_pid, ServerPid}] = ets:lookup(data, server_pid),
-  ok = msgbus_rpc_proxy_sup:stop_server(ServerPid).
+  ok = msgbus_rpc_proxy:stop_server(ServerPid).
 
 stop_client_test() ->
   [{client_pid, ClientPid}] = ets:lookup(data, client_pid),
-  ok = msgbus_rpc_proxy_sup:stop_server(ClientPid).
+  ok = msgbus_rpc_proxy:stop_server(ClientPid).
