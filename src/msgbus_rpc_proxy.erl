779fddf494729813367a97f6f93a9cc9dd12562f
shdxiang <shdxiang@qq.com>
2017-01-18 15:00:47 +0800

    rename files

--- src/msgbus_rpc_proxy.erl
+++ src/msgbus_rpc_proxy.erl
@@ -1,48 +1,189 @@
+%%%-------------------------------------------------------------------
+%%% @author shdxiang
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 08. Nov 2016 5:00 PM
+%%%-------------------------------------------------------------------
 -module(msgbus_rpc_proxy).
+-author("shdxiang").
 
--behaviour(supervisor).
+-behaviour(gen_server).
+
+-compile({parse_transform, lager_transform}).
+
+-define(NODEBUG, true).
+-include_lib("eunit/include/eunit.hrl").
 
 %% API
--export([start_link/0,
-  start_server/2,
-  start_client/2,
-  rpc/2,
-  stop_server/1,
-  stop_client/1]).
+-export([start_link/1]).
 
-%% Supervisor callbacks
--export([init/1]).
+%% gen_server callbacks
+-export([init/1,
+  handle_call/3,
+  handle_cast/2,
+  handle_info/2,
+  terminate/2,
+  code_change/3]).
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(SERVER, ?MODULE).
 
-%% ===================================================================
-%% API functions
-%% ===================================================================
+-define(REQ_TIMEOUT, 2000).
 
-start_link() ->
-  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+-record(state, {
+  sock,
+  handler
+}).
 
-start_server(Url, Handler) ->
-  supervisor:start_child(?MODULE, [{server, Url, Handler}]).
+%%%===================================================================
+%%% API
+%%%===================================================================
 
-start_client(Url, Handler) ->
-  supervisor:start_child(?MODULE, [{client, Url, Handler}]).
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(start_link(Args :: term()) ->
+  {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+start_link(Args) ->
+  gen_server:start_link(?MODULE, [Args], []).
 
-rpc(Pid, Data) ->
-  gen_server:call(Pid, {rpc, Data}).
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
 
-stop_server(Pid) ->
-  supervisor:terminate_child(?MODULE, Pid).
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+-spec(init(Args :: term()) ->
+  {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
+  {stop, Reason :: term()} | ignore).
+init([{server, Url, Handler}]) ->
+  lager:debug("init server: ~p", [Url]),
+  {ok, Sock} = enm:rep([{bind, Url}, {nodelay, true}]),
+  lager:debug("server: ~p", [Sock]),
+  {ok, #state{sock = Sock, handler = Handler}};
 
-stop_client(Pid) ->
-  supervisor:terminate_child(?MODULE, Pid).
+init([{client, Url, Handler}]) ->
+  lager:debug("init client: ~p", [Url]),
+%%  https://github.com/basho/enm/issues/7
+  {ok, Sock} = enm:req([{connect, Url}, {nodelay, true}]),
+  lager:debug("client: ~p", [Sock]),
+  {ok, #state{sock = Sock, handler = Handler}}.
 
-%% ===================================================================
-%% Supervisor callbacks
-%% ===================================================================
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
+    State :: #state{}) ->
+  {reply, Reply :: term(), NewState :: #state{}} |
+  {reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
+  {noreply, NewState :: #state{}} |
+  {noreply, NewState :: #state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
+  {stop, Reason :: term(), NewState :: #state{}}).
 
-init([]) ->
-  {ok, {{simple_one_for_one, 1, 5},
-    [{undefined, {msgbus_rpc_proxy_gs, start_link, []},
-      transient, 5000, worker, [msgbus_rpc_proxy]}]}}.
+handle_call({rpc, ReqData}, _From, #state{sock = Sock} = State) ->
+  lager:debug("rpc: ~p", [Sock]),
+  ok = enm:send(Sock, ReqData),
+
+  receive
+    {nnreq, Sock, RepData} ->
+      {reply, {ok, RepData}, State}
+  after
+    ?REQ_TIMEOUT -> {reply, {error, timeout}, State}
+  end;
+
+handle_call(_Request, _From, State) ->
+  {reply, ok, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(handle_cast(Request :: term(), State :: #state{}) ->
+  {noreply, NewState :: #state{}} |
+  {noreply, NewState :: #state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), NewState :: #state{}}).
+handle_cast(_Request, State) ->
+  {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+-spec(handle_info(Info :: timeout() | term(), State :: #state{}) ->
+  {noreply, NewState :: #state{}} |
+  {noreply, NewState :: #state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), NewState :: #state{}}).
+handle_info({nnrep, Sock, Data}, #state{sock = Sock, handler = Handler} = State) when is_pid(Handler) ->
+  lager:debug("receive a nnrep: ~p", [Sock]),
+  ok = enm:send(Sock, <<0>>),
+  Handler ! {rpc_proxy_data, Data},
+  {noreply, State};
+handle_info({nnrep, Sock, _Data}, #state{sock = Sock, handler = Handler} = State) ->
+  lager:debug("receive a nnrep: ~p, handler: ~p", [Sock, Handler]),
+  ok = enm:send(Sock, <<0>>),
+  {noreply, State};
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+-spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
+    State :: #state{}) -> term()).
+terminate(_Reason, #state{sock = Sock} = _State) ->
+  lager:debug("terminate", []),
+  enm:close(Sock),
+  ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+-spec(code_change(OldVsn :: term() | {down, term()}, State :: #state{},
+    Extra :: term()) ->
+  {ok, NewState :: #state{}} | {error, Reason :: term()}).
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
---
+++ src/msgbus_rpc_proxy_sup.erl
@@ -0,0 +1,48 @@
+-module(msgbus_rpc_proxy_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0,
+  start_server/2,
+  start_client/2,
+  rpc/2,
+  stop_server/1,
+  stop_client/1]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+start_server(Url, Handler) ->
+  supervisor:start_child(?MODULE, [{server, Url, Handler}]).
+
+start_client(Url, Handler) ->
+  supervisor:start_child(?MODULE, [{client, Url, Handler}]).
+
+rpc(Pid, Data) ->
+  gen_server:call(Pid, {rpc, Data}).
+
+stop_server(Pid) ->
+  supervisor:terminate_child(?MODULE, Pid).
+
+stop_client(Pid) ->
+  supervisor:terminate_child(?MODULE, Pid).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+  {ok, {{simple_one_for_one, 1, 5},
+    [{undefined, {msgbus_rpc_proxy_gs, start_link, []},
+      transient, 5000, worker, [msgbus_rpc_proxy]}]}}.
