6a9f5ee959fb461acd402f8f47d30c46da1f53e8
shdxiang <shdxiang@qq.com>
2017-03-01 20:34:32 +0800

    format codes

--- src/yberpc_client_selector.erl
+++ src/yberpc_client_selector.erl
@@ -5,29 +5,30 @@
 
 %% input with a list of clients, and return with a Client
 %% if the list is empty, return undefined
--spec(select_one(Clients::list())-> Client::tuple() | undefined).
+-spec(select_one(Clients :: list()) -> Client :: tuple() | undefined).
 select_one(Clients) ->
-    TotalWeight = lists:foldl(
-        fun({_, _, Weight, _}, CurWeight) -> 
-            CurWeight + Weight
-        end, 0, Clients),
-    RandomWeight = 
-        case TotalWeight of
-            0 -> 0;
-            _ -> random:uniform(TotalWeight)
-        end,
-    select_one(Clients, 0, RandomWeight).
+  TotalWeight = lists:foldl(
+    fun({_, _, Weight, _}, CurWeight) ->
+      CurWeight + Weight
+    end, 0, Clients),
+  RandomWeight =
+    case TotalWeight of
+      0 -> 0;
+      _ -> random:uniform(TotalWeight)
+    end,
+  select_one(Clients, 0, RandomWeight).
 
 select_one([], _, _) ->
-    undefined;
+  undefined;
 select_one([Client], _, _) ->
-    lager:debug("client is selected:~p", [Client]),
-    Client;
-select_one([Client|Tail], CurWeight, RandomWeight) ->
-    {_, _, Weight, _} =  Client,
-    case (Weight + CurWeight >= RandomWeight) of
-        true -> 
-            lager:debug("client is selected:~p", [Client]),
-            Client;
-        _ -> select_one(Tail, Weight + CurWeight, RandomWeight)
-    end.
+  lager:debug("client is selected: ~p", [Client]),
+  Client;
+select_one([Client | Tail], CurWeight, RandomWeight) ->
+  {_, _, Weight, _} = Client,
+  case (Weight + CurWeight >= RandomWeight) of
+    true ->
+      lager:debug("client is selected: ~p", [Client]),
+      Client;
+    _ ->
+      select_one(Tail, Weight + CurWeight, RandomWeight)
+  end.
